56cff37e9cddeea30c142bb647cf6b3ab38bb0d7
maldkun <matthewconsulta842@gmail.com>
2023-04-07 19:23:22 +0800

    2nd commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Seeder;
 
 class CreateUsersTable extends Migration
 {
@@ -19,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('isAdmin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'id' => 1,
+            'name' => "Matt Admin",
+            'email' => "geraldxconsulta@gmail.com",
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'isAdmin' => 1
+        ]);
+    }
+}
